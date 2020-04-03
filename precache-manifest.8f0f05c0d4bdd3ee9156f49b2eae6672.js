79361dc7f670fb21ce7a481145622ce3ed6ea961
reynaldpn <reynaldpn@gmail.com>
2020-04-03 20:50:38 +0700

    Updates

---
+++ precache-manifest.8f0f05c0d4bdd3ee9156f49b2eae6672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d087dfe90b7db42e94c66cb328bee7",
+    "url": "/dedicated-time-winner/index.html"
+  },
+  {
+    "revision": "74dd8244a8690ba92085",
+    "url": "/dedicated-time-winner/static/css/main.4e5ef7b0.chunk.css"
+  },
+  {
+    "revision": "9a9dbe7507c5c52d41b9",
+    "url": "/dedicated-time-winner/static/js/2.2816a81c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dedicated-time-winner/static/js/2.2816a81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74dd8244a8690ba92085",
+    "url": "/dedicated-time-winner/static/js/main.841f4a7f.chunk.js"
+  },
+  {
+    "revision": "e857fb5ee1c12658a4ee",
+    "url": "/dedicated-time-winner/static/js/runtime-main.568784e0.js"
+  }
+]);
